79094e9edcfe889e01eaf67af9232c77773d7a16
Grayson Gales <graysongales15@gmail.com>
2026-01-20 22:04:16 -0500

    added buttons styling

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -7,18 +7,24 @@ import "./Home.scss";
 export default function Home() {
   return (
     <>
-      <section className="home-head">
-        <img className="headshot" src={headshot} alt="Headshot photo"></img>
+      <section className="titles">
         <h1>{about.name}</h1>
+        <h3>{about.location}</h3>
       </section>
-      <section>
-        <p>{about.shortBio}</p>
-      </section>
-      <section>
-        <Link to="/projects">View Projects</Link>
-        <Link to="/about">About Me</Link>
-        <Link to="/">Download Resume</Link>
-        <Link to="/connect">Contact</Link>
+      <p className="short-bio">{about.shortBio}</p>
+      <section className="btn-container">
+        <Link className="btn" to="/projects">
+          View Projects
+        </Link>
+        <Link className="btn" to="/about">
+          About Me
+        </Link>
+        <Link className="btn" to="/">
+          Download Resume
+        </Link>
+        <Link className="btn" to="/connect">
+          Contact
+        </Link>
       </section>
     </>
   );
