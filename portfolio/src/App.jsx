95814c5a3b7e8fed74e9468b7797a75694d881ea
Grayson Gales <graysongales15@gmail.com>
2026-01-20 20:42:16 -0500

    created Home.jsx, changed App style to scss

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,10 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header/Header";
-import "./App.css";
+import Home from "./components/Home/Home";
+import "./App.scss";
 
 function Projects() {
   return <h1>Projects</h1>;
-}
-
-function Home() {
-  return <h1>Home</h1>;
 }
 
 function About() {
@@ -23,7 +20,7 @@ function App() {
     <>
       <Header />
 
-      <main>
+      <main className="app-container">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
